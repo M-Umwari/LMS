169dfd744007028b67132ab7a7318e6f0746d8a6
M-Umwari <m.umwari@alustudent.com>
2023-12-18 18:03:40 +0200

    starting react

---
+++ lms/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Home = props => {
+  return (
+    <div>
+        <header>
+            
+        </header>
+    </div>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
